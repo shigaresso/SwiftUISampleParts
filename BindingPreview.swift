480e7cbc8acb4f3f2221f4a1e42d877146b73826
shigaresso <70837100+shigaresso@users.noreply.github.com>
2021-08-02 22:57:18 +0900

    Binding 変数利用時の Preview でのエラー解決方法の追加

--- BindingPreview.swift
+++ BindingPreview.swift
@@ -1,3 +1,25 @@
+// 方法その１
+
+import SwiftUI
+
+struct BindingPreview: View {
+    /*
+     @Binding を利用する場合、Preview 側の引数を .constant() の引数にする
+    */
+    @Binding var BindingString: String
+    var body: some View {
+        Text(BindingString)
+    }
+}
+
+struct BindingPreview_Previews: PreviewProvider {
+    static var previews: some View {
+        BindingPreview(BindingString: .constant("Hello, World!"))
+    }
+}
+
+// 方法その２
+
 import SwiftUI
 
 struct BindingPreview: View {
